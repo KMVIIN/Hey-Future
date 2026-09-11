b0c96b597a4f49fbfdfdee86ff18de00ae1c08fe
KMVIIN <kanjnsiri.m@gmail.com>
2026-09-12 04:47:28 +0700

    Update notifications.ts

--- lib/notifications.ts
+++ lib/notifications.ts
@@ -49,7 +49,6 @@ export async function showRawNotification(title: string, body: string, tag = 'fu
     await registration.showNotification(title, {
       body,
       tag,
-      renotify: true,
       data: { url: '/' },
       requireInteraction: false,
     });
